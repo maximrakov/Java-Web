0ce9d91351b12a718e4ec1aa8e6eb8d55b79d1f6
maximrakov <78982680+maximrakov@users.noreply.github.com>
2022-07-20 21:49:52 +0300

    ref: delete usless staff:

--- ajax/src/main/java/ru/itmo/webmail/model/database/DatabaseUtils.java
+++ ajax/src/main/java/ru/itmo/webmail/model/database/DatabaseUtils.java
@@ -1,14 +1,20 @@
 package ru.itmo.webmail.model.database;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.mariadb.jdbc.MariaDbDataSource;
 import javax.sql.DataSource;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseUtils {
+    private static PropertiesConfiguration config = new PropertiesConfiguration();
     public static Connection getDataSource() {
         return DataSourceHolder.connection;
     }
@@ -16,9 +22,16 @@ public class DatabaseUtils {
         private static Connection connection;
 
         static {
-            final String URL = "jdbc:postgresql://localhost:5432/webLab5";
-            final String USERNAME = "postgres";
-            final String PASSWORD = "pqqk9a9s";
+
+            try {
+                File file = new File("");
+                config.load("..\\webapps\\ROOT\\WEB-INF\\classes\\application.properties");
+            } catch (ConfigurationException e) {
+                throw new RuntimeException(e);
+            }
+            final String URL = config.getString("database.url");
+            final String USERNAME = config.getString("database.user");
+            final String PASSWORD = config.getString("database.password");
             try {
                 Class.forName("org.postgresql.Driver");
             } catch (ClassNotFoundException e) {
